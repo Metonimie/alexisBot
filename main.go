bf1b7dfe280e93fcdf6e144604307f651d37d60a
Denis Nuțiu <denis.nutiu@gmail.com>
2017-05-23 22:38:52 +0300

    Basic server with let's encrypt

--- main.go
+++ main.go
@@ -4,29 +4,45 @@ import (
     "net/http"
     "log"
     "fmt"
+    "golang.org/x/crypto/acme/autocert"
+    "crypto/tls"
 )
 
+const host = "denisnutiu.me"
 const ip = "" // empty means it binds to everything.
-const port = "8080"
+const port = "443" // Facebook wants us to be "secure". FaCeeBoK WanTs Us To bE sEcUrE.
 
-// The webhook functions receives challenges from Facebook.
-func webhook(w http.ResponseWriter, r * http.Request) {
-    if r.Method == "POST" {
+// The Webhook functions receives challenges from Facebook.
+func Webhook(w http.ResponseWriter, r * http.Request) {
+    //if r.Method == "POST" {
         fmt.Println(r.Body)
-    } else {
-        // If we don't have POST then we don't POST!!
-        http.Error(w, "Invalid request method.", 405)
-    }
+    //} else {
+    //    // If we don't have POST then we don't POST!!
+    //    http.Error(w, "Invalid request method.", 405)
+    //}
 }
 
 func main() {
+    // Let's Encrypt Certificate Manager.
+    certManager := autocert.Manager{
+        Prompt:     autocert.AcceptTOS,
+        HostPolicy: autocert.HostWhitelist(host), //your domain here
+        Cache:      autocert.DirCache("certs"), //folder for storing certificates
+    }
+
     // Handle functions
-    http.HandleFunc("/webhook", webhook)
+    http.HandleFunc("/webhook", Webhook)
 
 
+    server := &http.Server{
+        Addr: ip + ":" + port,
+        TLSConfig: &tls.Config{
+            GetCertificate: certManager.GetCertificate,
+        },
+    }
 
     log.Println("Hello world, I think, therefore I am.")
-    log.Println("Running on", "http://"+ ip + ":" + port)
     // Start the server and log.
-    log.Fatalln(http.ListenAndServe(ip + ":" + port, nil))
+    //log.Fatalln(http.ListenAndServe(ip + ":" + port, nil))
+    log.Fatalln(server.ListenAndServeTLS( "", ""))
 }
